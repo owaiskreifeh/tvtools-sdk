130444d77b8d2cae7f1c27db0899cdeb60967f8d
owaiskreifeh <Owais.AbuKreefeh@mbc.net>
2020-10-20 21:44:43 +0300

    moving chii port to env

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var path = require("path");
 var fs = require("fs");
-var app = require("../app");
+var chiiPort = process.env["CHII_PORT"];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var chiiPort = app.get("chiiPort")
-  res.render('index', { title: 'Shahid TV devtools', chiiPort });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Shahid TV devtools", chiiPort });
 });
 
-router.get('/sdk', function(req, res, next) {
-var chiiPort = app.get("chiiPort");
-  var scriptTemplatePath = path.resolve("./public/sdk/sdk.compiled.js");
-  var scriptTemplate = fs.readFileSync(scriptTemplatePath, 'utf8');
-  scriptTemplate.replace("{#PORT#}", chiiPort);
-  res.send(scriptTemplate)
+router.get("/sdk", function (req, res, next) {
+    var scriptTemplatePath = path.resolve("./public/sdk/sdk.compiled.js");
+    var scriptTemplate = fs.readFileSync(scriptTemplatePath, "utf8");
+    scriptTemplate.replace("{#PORT#}", chiiPort);
+    res.send(scriptTemplate);
 });
 
 module.exports = router;
